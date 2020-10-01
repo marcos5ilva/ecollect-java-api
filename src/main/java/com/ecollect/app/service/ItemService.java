a8768e2894a9db77fafc13e3a5e706677be17504
Marcos Silva <marcos.silva79@gmail.com>
2020-10-01 13:22:12 -0400

    feat: add Service annotation at the ItemService class

--- src/main/java/com/ecollect/app/service/ItemService.java
+++ src/main/java/com/ecollect/app/service/ItemService.java
@@ -3,7 +3,9 @@ package com.ecollect.app.service;
 import com.ecollect.app.dao.ItemDao;
 import com.ecollect.app.module.Item;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ItemService {
     private final ItemDao itemDao;
 
